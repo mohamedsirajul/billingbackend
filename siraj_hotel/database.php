4ca06d1a63713f27a716d4adfb5cf787f6d272e9
mohamedsirajul <mohamedsirajulmuneer88@gmail.com>
2023-07-20 14:22:17 +0530

    credentials updated

--- siraj_hotel/database.php
+++ siraj_hotel/database.php
@@ -5,10 +5,10 @@ header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Content-Type: application/json; charset=UTF-8");
 
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASS', '');
-define('NAME', 'sirajhotel');
+define('HOST', 'ls-b905a3d8faa4200463b96db2e201a7af41d2615c.cjdiwhzr1pzl.ap-south-1.rds.amazonaws.com');
+define('USER', 'dbmasteruser');
+define('PASS', 'GGrS&2FI.aofUYW!pv8!Cr2rS^Y[UAS.');
+define('NAME', 'dbmaster');
 
 $db = new mysqli(HOST ,USER ,PASS ,NAME);
 if ($db->connect_errno) {
